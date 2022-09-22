45a37e344d0301308cb61d1583a7ff8707c99b8a
Joel Ortega <joel.rosillo@gmail.com>
2022-09-22 20:45:05 +0200

    22/9/22

--- Entornos Servidor/Acme/GesPresta/Empleados.aspx
+++ Entornos Servidor/Acme/GesPresta/Empleados.aspx
@@ -15,9 +15,62 @@
         <div>
             <uc1:Cabecera ID="Cabecera1" runat="server" />
         </div>
-         <div>
-                   
-		   </div>
+
+             <div class="tablagen">
+              <div class="fila">
+                <div class="col">Código Empleado :</div>
+                <div class="col2"><asp:TextBox ID="TextBox1" runat="server"></asp:TextBox></div>
+              </div>
+              <div class="fila">
+                <div class="col">NIF :</div>
+                <div class="col2"><asp:TextBox ID="TextBox2" runat="server"></asp:TextBox></div>
+              </div>
+              <div class="fila">
+                <div class="col">Apellidos y Nombre :</div>
+                <div class="col2"><asp:TextBox ID="TextBox3" runat="server" Width="360px"></asp:TextBox></div>
+              </div>
+              <div class="fila">
+                <div class="col">Dirección :</div>
+                <div class="col2"><asp:TextBox ID="TextBox4" runat="server" Width="410px"></asp:TextBox></div>
+              </div>
+              <div class="fila">
+                <div class="col">Teléfonos :</div>
+                <div class="col2"><asp:TextBox ID="TextBox5" runat="server" Width="410px"></asp:TextBox></div>
+              </div>
+              <div class="fila">
+                <div class="col">Fecha de Nacimiento :</div>
+                <div class="col2"><asp:TextBox ID="TextBox6" runat="server" Width="275px"></asp:TextBox></div>
+              </div>
+              <div class="fila">
+                <div class="col">Fecha de Ingreso :</div>
+                <div class="col2"><asp:TextBox ID="TextBox7" runat="server"></asp:TextBox></div>
+              </div>
+              <div class="fila">
+                <div class="col">Sexo :</div>
+                <div class="col2">
+                    <asp:RadioButtonList ID="RadioButtonList1" runat="server" RepeatDirection="Horizontal">
+                        <asp:ListItem Selected="True">Hombre</asp:ListItem>
+                        <asp:ListItem>Mujer</asp:ListItem>
+                    </asp:RadioButtonList>
+                </div>
+               </div>
+               <div class="fila">
+                <div class="col">Departamento :</div>
+                <div class="col2">
+                    <asp:DropDownList ID="DropDownList1" runat="server">
+                        <asp:ListItem>Investigación</asp:ListItem>
+                        <asp:ListItem>Desarrollo</asp:ListItem>
+                        <asp:ListItem>Innovación</asp:ListItem>
+                        <asp:ListItem>Administración</asp:ListItem>
+                    </asp:DropDownList>
+                </div>
+                </div>
+                <div class="fila">
+                 <div class="col"></div>  
+                 <div class="col2"><asp:Button ID="Button1" runat="server" Text="Button" /></div>
+              </div>
+            </div>
+
     </form>
 </body>
 </html>
--- Entornos Servidor/Acme/GesPresta/Empleados.aspx.designer.cs
+++ Entornos Servidor/Acme/GesPresta/Empleados.aspx.designer.cs
@@ -31,5 +31,95 @@ namespace GesPresta
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::GesPresta.Cabecera Cabecera1;
+
+        /// <summary>
+        /// Control TextBox1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+
+        /// <summary>
+        /// Control TextBox2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+
+        /// <summary>
+        /// Control TextBox3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox3;
+
+        /// <summary>
+        /// Control TextBox4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox4;
+
+        /// <summary>
+        /// Control TextBox5.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox5;
+
+        /// <summary>
+        /// Control TextBox6.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox6;
+
+        /// <summary>
+        /// Control TextBox7.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox7;
+
+        /// <summary>
+        /// Control RadioButtonList1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButtonList RadioButtonList1;
+
+        /// <summary>
+        /// Control DropDownList1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+
+        /// <summary>
+        /// Control Button1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
     }
 }
